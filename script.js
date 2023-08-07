7078f1150b2e390618494db0dc480aecfba2db68
Kamil <perlegus@gmail.com>
2023-08-07 15:12:26 +0200

    added borderColor changes

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const border = document.querySelector('.box');
+
+const colors = ['#ff0000', '#1656f9', '#56f916', '#F97316']
+let currentColorIndex = 0;
+
+  setInterval(() => {
+        border.style.borderColor = colors[currentColorIndex];
+        currentColorIndex = (currentColorIndex + 1) % colors.length;
+        }, 3000)
+
 console.log('Hello from Nerdbord!')
